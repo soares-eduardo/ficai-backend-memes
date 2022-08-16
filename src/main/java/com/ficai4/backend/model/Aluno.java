896e1debff6d07b2365b0e213a034062ff04492a
Eduardo Soares <eduardo.andre.soares@hp.com>
2022-08-16 11:38:48 -0300

    code entities according to db diagram

--- src/main/java/com/ficai4/backend/model/Aluno.java
+++ src/main/java/com/ficai4/backend/model/Aluno.java
@@ -1,5 +1,90 @@
 package com.ficai4.backend.model;
 
+import java.time.LocalDate;
+
 public class Aluno {
     
+    //TODO Efetuar validação dos campos nesta camada
+
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private String responsavelLegal;
+    private String beneficios;
+    private String situacao;
+    private Endereco endereco;
+    private LocalDate dataNascimento;
+    private Escola escola;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getResponsavelLegal() {
+        return responsavelLegal;
+    }
+
+    public String getBeneficios() {
+        return beneficios;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public Escola getEscola() {
+        return escola;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setResponsavelLegal(String responsavelLegal) {
+        this.responsavelLegal = responsavelLegal;
+    }
+
+    public void setBeneficios(String beneficios) {
+        this.beneficios = beneficios;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setEscola(Escola escola) {
+        this.escola = escola;
+    }
 }
--- src/main/java/com/ficai4/backend/model/Usuario.java
+++ src/main/java/com/ficai4/backend/model/Usuario.java
@@ -2,4 +2,41 @@ package com.ficai4.backend.model;
 
 public class Usuario {
     
+    private String cpf;
+    private String nome;
+    private String perfil;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String cpf, String nome, String perfil) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.perfil = perfil;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
 }
